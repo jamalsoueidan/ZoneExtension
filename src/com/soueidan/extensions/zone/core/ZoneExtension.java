532bb72f1847b320bf2fc1b7c25d8e3a2d0ea198
Jamal Soueidan <jamal@soueidan.com>
2011-08-29 20:45:08 +0200

    Init

---
+++ src/com/soueidan/extensions/zone/core/ZoneExtension.java
@@ -0,0 +1,32 @@
+package com.soueidan.extensions.zone.core;
+
+import com.smartfoxserver.v2.core.SFSEventType;
+import com.smartfoxserver.v2.extensions.*;
+import com.soueidan.extensions.zone.events.UserLoginEventHandler;
+import com.soueidan.extensions.zone.events.UserZoneJoinEventHandler;
+
+public class ZoneExtension extends SFSExtension {
+
+	
+	public static final String ROOM_NAME = "room";
+	public static final String USER_ID = "user_id";
+	public static final String USER_REGISTERED = "user_registered";
+	public static final String USER_SESSION = "session";
+	
+	@Override
+	public void init() {
+		
+		trace("ZoneExtension init");
+		
+		addEventHandler(SFSEventType.USER_LOGIN, UserLoginEventHandler.class);
+		addEventHandler(SFSEventType.USER_JOIN_ZONE, UserZoneJoinEventHandler.class);
+
+	}
+	
+	@Override
+	public void destroy()
+	{
+	    super.destroy();
+	    trace("ZoneExtension destroy");
+	}
+}
---
+++ src/com/soueidan/extensions/zone/events/UserZoneJoinEventHandler.java
@@ -0,0 +1,42 @@
+package com.soueidan.extensions.zone.events;
+
+import java.util.Arrays;
+
+import com.smartfoxserver.bitswarm.sessions.ISession;
+import com.smartfoxserver.v2.core.ISFSEvent;
+import com.smartfoxserver.v2.core.SFSEventParam;
+import com.smartfoxserver.v2.entities.Room;
+import com.smartfoxserver.v2.entities.User;
+import com.smartfoxserver.v2.entities.variables.SFSUserVariable;
+import com.smartfoxserver.v2.entities.variables.UserVariable;
+import com.smartfoxserver.v2.exceptions.SFSException;
+import com.smartfoxserver.v2.extensions.BaseServerEventHandler;
+import com.soueidan.extensions.zone.core.ZoneExtension;
+
+public class UserZoneJoinEventHandler extends BaseServerEventHandler {
+
+	@Override
+	public void handleServerEvent(ISFSEvent event) throws SFSException {
+		User user = (User) event.getParameter(SFSEventParam.USER);
+		ISession session = user.getSession();
+		
+		UserVariable userId = new SFSUserVariable("dbid", session.getProperty(ZoneExtension.USER_ID));
+		userId.setHidden(true);
+		
+		UserVariable userRegistered = new SFSUserVariable("registered", session.getProperty(ZoneExtension.USER_REGISTERED));
+		userRegistered.setHidden(false);
+		
+		java.util.List<UserVariable> vars = Arrays.asList(userId, userRegistered);
+		getApi().setUserVariables(user, vars);
+		
+		String roomName = session.getProperty(ZoneExtension.ROOM_NAME).toString();
+		
+		Room lobby = getParentExtension().getParentZone().getRoomByName(roomName);
+		
+		if (lobby == null) {
+			throw new SFSException(lobby + " room was not found!");
+		}
+		
+		getApi().joinRoom(user, lobby);
+	}
+}
